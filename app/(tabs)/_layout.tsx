3109954205412f1877f22ff11bb435896eb387dd
Pedro Marques <pedromarques391@gmail.com>
2025-06-11 15:32:31 -0300

    feat: adding icons in tabs.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,25 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: 'coral',
+      headerShown: false
+    }}>
+      <Tabs.Screen name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => {
+            return focused
+              ? <FontAwesome5 name='home' size={24} color={color} />
+              : <AntDesign name='home' size={24} color={color} />
+          }
+        }}
+
+      />
       <Tabs.Screen name="login" options={{ title: 'Login' }} />
     </Tabs>
   )
