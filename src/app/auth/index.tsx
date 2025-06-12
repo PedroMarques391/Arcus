aec69cbad31f6b9018bca10da387a8ea346bb7af
Pedro Marques <pedromarques391@gmail.com>
2025-06-12 14:53:27 -0300

    feat: Finalizing auth page.

--- src/app/auth/index.tsx
+++ src/app/auth/index.tsx
@@ -16,12 +16,13 @@ export default function AuthPage() {
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             style={styles.container}
         >
-
             <View style={styles.content}>
-                <Image
-                    style={styles.image}
-                    source={require('../../../assets/images/app/logo.png')}
-                />
+                <View style={styles.logoView}>
+                    <Image
+                        source={require('../../../assets/images/app/logo.png')}
+                    />
+                    <Text style={styles.appName}>Arcus</Text>
+                </View>
                 <Text style={styles.title}>{screen === "signUp" ? 'Venha fazer parte' : 'Bem vindo de volta'}</Text>
                 {screen === "signIn"
                     ? <SignIn />
@@ -48,21 +49,33 @@ const styles = StyleSheet.create({
         backgroundColor: '#f5f5f5',
         justifyContent: "flex-start",
     },
-    image: {
+    logoView: {
         alignSelf: 'center',
-
+        position: "relative",
+        width: 300,
+        height: 200
+    },
+    appName: {
+        position: "absolute",
+        top: 170,
+        left: 84,
+        letterSpacing: 10,
+        fontSize: 20,
+        fontFamily: 'serif',
+        fontWeight: "700",
+        color: '#5C6BF5'
     },
     content: {
         padding: 20,
-        justifyContent: "center",
-        gap: 10
+        justifyContent: "flex-start",
+        gap: 10,
     },
     title: {
         fontSize: 20,
         fontWeight: "bold",
         color: "#22223b",
         textAlign: "center",
-        marginBottom: 14,
+        marginVertical: 15,
         letterSpacing: 1,
     }
 })
