3b32cec74e5190373df16370081f843870ab8b58
Pedro Marques <pedromarques391@gmail.com>
2025-06-13 16:14:42 -0300

    feat: using user and loading to control screens.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { AuthProvider } from "@/context/AuthContext";
-import { Stack, useRouter } from "expo-router";
+import { useAuth } from "@/hook/useAuth";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { ReactNode, useEffect } from "react";
 import { MD3LightTheme, PaperProvider } from "react-native-paper";
 
@@ -8,14 +9,21 @@ interface IRootInterface {
 }
 
 function RouteGuard({ children }: IRootInterface) {
+  const { user, isLoadingUser } = useAuth()
   const router = useRouter();
-  const isAuth = false;
+  const segments = useSegments()
 
   useEffect(() => {
-    if (!isAuth) {
-      return router.replace('/auth')
+    const inAuthGroup = segments[0] === 'auth'
+    if (!user && !inAuthGroup && !isLoadingUser) {
+      router.replace('/auth')
+      return
     }
-  })
+    if (user && inAuthGroup && !isLoadingUser) {
+      router.replace('/')
+      return
+    }
+  }, [user, segments])
 
   return <>{children}</>
 }
