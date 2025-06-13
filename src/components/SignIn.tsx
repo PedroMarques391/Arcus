01ef0b09d70701ca175751e6d5cc5179fa1df6bb
Pedro Marques <pedromarques391@gmail.com>
2025-06-13 16:10:35 -0300

    feat: calling functions signIn and signUp in screens.

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,27 +1,78 @@
-import React from "react";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
 
-export function SignIn(): React.JSX.Element {
+type TSignInProps = {
+    singIn: (email: string, password: string) => Promise<string | null>
+}
+
+export function SignIn({ singIn }: TSignInProps): React.JSX.Element {
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [error, setError] = useState<string | null>('')
+    const theme = useTheme()
+    const router = useRouter()
+
+    async function handleAuth() {
+        if (!email || !password) {
+            setError('Por favor, preencha todos os campos.')
+            setTimeout(() => setError(null), 5000)
+            return
+        }
+
+        if (password.length < 6) {
+            setError('A senha deve ter pelo menos 6 caracteres.')
+            setTimeout(() => setError(null), 5000)
+            return
+        }
+
+        setError(null)
+
+        try {
+            const error = await singIn(email, password)
+
+
+            if (error) {
+                return setError(error)
+            }
+
+            router.replace('/')
+
+
+        } catch (error) {
+            console.error('[handleAuth - SignIn] - Erro ao acessar conta.', error)
+        }
+    }
+
     return (
         <>
             <TextInput
                 label='email'
+                value={email}
                 placeholder="email@gmail.com"
                 keyboardType="email-address"
                 autoCapitalize="none"
                 mode="outlined"
+                onChangeText={(text) => setEmail(text)}
             />
             <TextInput
                 label='senha'
+                value={password}
                 placeholder="Sua senha..."
                 keyboardType="default"
                 autoCapitalize="none"
                 mode="outlined"
+                onChangeText={(text) => setPassword(text)}
                 secureTextEntry
             />
 
+            {error && <Text
+                style={{ color: theme.colors.error }}
+                variant="bodyMedium">{error}</Text>}
+
             <Button
+                onPress={handleAuth}
                 style={styles.button}
                 mode="contained">Entrar</Button>
         </>
@@ -31,5 +82,5 @@ export function SignIn(): React.JSX.Element {
 const styles = StyleSheet.create({
     button: {
         marginTop: 20
-    }
+    },
 })
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,34 +1,86 @@
-import React from "react";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
 
-export function SignUp(): React.JSX.Element {
+type TSignUpProps = {
+    singUp: (name: string, email: string, password: string) => Promise<string | null>
+}
+
+export function SignUp({ singUp }: TSignUpProps): React.JSX.Element {
+    const [name, setName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [error, setError] = useState<string | null>('')
+    const theme = useTheme()
+    const router = useRouter()
+
+    async function handleAuth() {
+        if (!email || !password || !name) {
+            setError('Por favor, preencha todos os campos.')
+            setTimeout(() => setError(null), 5000)
+            return
+        }
+
+        if (password.length < 6) {
+            setError('A senha deve ter pelo menos 6 caracteres.')
+            setTimeout(() => setError(null), 5000)
+            return
+        }
+
+        setError(null)
+        try {
+            const error = await singUp(name, email, password)
+
+            if (error) {
+                return setError(error)
+            }
+
+            router.replace('/')
+
+
+        } catch (error) {
+            console.error('[handleAuth - SignUp] - Erro ao criar conta.', error)
+        }
+
+    }
     return (
         <>
             <TextInput
                 label='nome'
+                onChangeText={(text) => setName(text)}
                 placeholder="John Doe"
                 keyboardType="default"
                 autoCapitalize="none"
                 mode="outlined"
+                value={name}
             />
             <TextInput
                 label='email'
+                onChangeText={(text) => setEmail(text)}
                 placeholder="email@gmail.com"
                 keyboardType="email-address"
                 autoCapitalize="none"
                 mode="outlined"
+                value={email}
             />
             <TextInput
                 label='senha'
+                onChangeText={(text) => setPassword(text)}
                 placeholder="Sua senha..."
                 keyboardType="default"
                 autoCapitalize="none"
                 mode="outlined"
+                value={password}
                 secureTextEntry
             />
 
+            {error && <Text
+                style={{ color: theme.colors.error }}
+                variant="bodyMedium">{error}</Text>}
+
             <Button
+                onPress={handleAuth}
                 style={styles.button}
                 mode="contained">
                 Criar Conta
