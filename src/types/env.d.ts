adcc71af222899a72e39dd7750dc594b8294db31
Pedro Marques <pedromarques391@gmail.com>
2025-06-15 21:05:53 -0300

    env: adding DB_ID and HABITS_COLLECTIONS_ID in env types.

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,5 +3,7 @@ declare namespace NodeJS {
         EXPO_PUBLIC_APPWRITE_ENDPOINT: string
         EXPO_PUBLIC_APPWRITE_PROJECT: string
         EXPO_PUBLIC_APPWRITE_PLATAFORM: string
+        EXPO_PUBLIC_DB_ID: string
+        EXPO_PUBLIC_HABITS_COLLECTION_ID: string
     }
 }
