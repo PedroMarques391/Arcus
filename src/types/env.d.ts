02a316da1720ecfe9a282a666895662e6a204c23
Pedro Marques <pedromarques391@gmail.com>
2025-06-16 23:28:37 -0300

    feat: adding EXPO_PUBLIC_HABITS_COMPLETIONS_ID in env.d.ts.

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -5,5 +5,6 @@ declare namespace NodeJS {
         EXPO_PUBLIC_APPWRITE_PLATAFORM: string
         EXPO_PUBLIC_DB_ID: string
         EXPO_PUBLIC_HABITS_COLLECTION_ID: string
+        EXPO_PUBLIC_HABITS_COMPLETIONS_ID: string
     }
 }
