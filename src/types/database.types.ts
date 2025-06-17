8c8fa104700c004fa14c0c80ff39ca904bf7a422
Pedro Marques <pedromarques391@gmail.com>
2025-06-17 16:22:49 -0300

    feat: creating IHabitCompletions interface.

--- src/types/database.types.ts
+++ src/types/database.types.ts
@@ -11,8 +11,22 @@ interface IHabits extends Models.Document {
 
 
 }
+interface IHabitCompletions extends Models.Document {
+    habit_id: string,
+    user_id: string,
+    title: string,
+    description: string,
+    frequency: string,
+    streak_count: number,
+    last_completed: string,
+    created_at: string
+
+
+}
+
 
 
 export type {
-    IHabits
+    IHabitCompletions, IHabits
 };
+
