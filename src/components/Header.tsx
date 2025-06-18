0468807c9c46ea676d76585ec61b16f0ecb2649e
Pedro Marques <pedromarques391@gmail.com>
2025-06-18 19:31:58 -0300

    style: adding more padding to the IOS plataform.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         paddingHorizontal: 16,
-        paddingTop: Platform.OS === 'ios' ? 40 : 40,
+        paddingTop: Platform.OS === 'ios' ? 60 : 40,
         paddingBottom: 10,
         borderBottomWidth: 0.5,
         borderBottomColor: '#eee',
