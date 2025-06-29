f9601323e94e130f8da9fce70830c3a9788a6866
Pedro Marques <pedromarques391@gmail.com>
2025-06-29 00:49:24 -0300

    feat: creating modal to edit profile.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,17 +1,207 @@
-import { Modal as PaperModal, Portal, Text } from "react-native-paper";
+import { account, BUCKET_ID, storage } from '@/database/appwrite';
+import { useAuth } from '@/hook/useAuth';
+import { useTheme } from '@/hook/useTheme';
+import { FontAwesome } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
+import * as ImagePicker from 'expo-image-picker';
+import React, { useEffect, useState } from 'react';
+import {
+    Image,
+    Modal,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from 'react-native';
+import { ID } from 'react-native-appwrite';
+import { Button, TextInput, useTheme as usePaperTheme } from 'react-native-paper';
 
 interface IModalProps {
     showModal: boolean;
-    hideModal: () => void
+    hideModal: () => void;
 }
 
-export default function Modal({ showModal, hideModal }: IModalProps): React.JSX.Element {
-    const containerStyle = { backgroundColor: 'white', padding: 20 };
-    return (
-        <Portal>
-            <PaperModal visible={showModal} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-                <Text>Example Modal.  Click outside this area to dismiss.</Text>
-            </PaperModal>
-        </Portal>
-    )
+type TNativeFile = {
+    name: string,
+    type: string,
+    size: number,
+    uri: string
 }
+
+export default function EditProfileModal({
+    showModal,
+    hideModal,
+}: IModalProps): React.JSX.Element {
+    const { user, getUser } = useAuth()
+    const [name, setName] = useState<string>(user?.name || '');
+    const [currentImageUrl, setCurrentImageUrl] = useState<string | null>(null);
+    const paperTheme = usePaperTheme();
+    const { theme } = useTheme();
+
+    useEffect(() => {
+        if (user?.prefs?.photo) {
+            setCurrentImageUrl(user.prefs.photo);
+        } else {
+            setCurrentImageUrl(null);
+        }
+    }, []);
+
+    async function prepareNativeFile(asset: ImagePicker.ImagePickerAsset): Promise<TNativeFile> {
+        console.log('[modal: prepareNativeFile]', asset)
+        try {
+            const url = new URL(asset.uri);
+            return {
+                name: url.pathname.split('/').pop()!,
+                type: asset.mimeType!,
+                size: asset.fileSize!,
+                uri: url.href,
+            }
+        } catch (error) {
+            console.error('[modal: prepareNativeFile] error', error)
+            throw error;
+        }
+    }
+    async function uploadImageAsync(asset: ImagePicker.ImagePickerAsset) {
+        try {
+            const response = await storage.createFile(
+                BUCKET_ID,
+                ID.unique(),
+                await prepareNativeFile(asset)
+            )
+
+            console.log('[modal: file uploaded]', response)
+
+            const imageUrl = storage.getFileView(BUCKET_ID!, response.$id);
+
+            await account.updatePrefs({ photo: imageUrl });
+
+            console.log('[modal: imageUrl]', imageUrl)
+
+            return imageUrl
+
+        } catch (error) {
+            console.error('[Modal - uploadImageAsync]: Erro ao enviar imagem:', error);
+        }
+    }
+
+    async function pickImage() {
+        let pickerResult = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        })
+
+        console.log('[modal: pickerResult]', { pickerResult })
+
+        await handleImagePicked(pickerResult)
+    }
+
+
+    async function handleImagePicked(pickerResult: ImagePicker.ImagePickerResult) {
+        try {
+            if (!pickerResult.canceled) {
+                await uploadImageAsync(pickerResult.assets[0]);
+                getUser();
+                setCurrentImageUrl(pickerResult.assets[0].uri);
+                alert('Foto Alterada com sucesso!')
+            }
+        } catch (error) {
+            console.log('[modal: handleImagePicker error]', error)
+            alert('Algo deu errado!')
+
+        }
+    }
+
+    async function handleUpdateProfile() {
+        try {
+            if (!name.trim()) {
+                alert('Por favor, preencha todos os campos.');
+                return;
+            }
+            await account.updateName(name);
+            getUser();
+            hideModal();
+            alert('Perfil atualizado com sucesso!');
+
+        } catch (error) {
+            console.log('[modal: handleUpdateProfile error]', error)
+            alert('Algo deu errado!')
+        }
+    }
+
+    return (
+        <Modal
+            transparent
+            animationType="fade"
+            visible={showModal}
+            onRequestClose={hideModal}
+        >
+            <BlurView intensity={30} tint={theme === 'dark' ? 'prominent' : 'dark'} style={styles.blurContainer}>
+                <View style={[styles.modalContent, { backgroundColor: paperTheme.colors.surfaceVariant }]}>
+                    <TouchableOpacity onPress={hideModal} style={{ position: 'absolute', top: 10, right: 10 }}>
+                        <FontAwesome name="close" size={28} color={paperTheme.colors.error} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={pickImage}
+                        style={{ alignItems: 'center', width: '80%', marginHorizontal: 'auto' }}>
+                        <Image
+                            source={currentImageUrl ? { uri: `${currentImageUrl}` } : require('../../assets/images/app/camIcon.png')}
+                            resizeMode='cover'
+                            style={[styles.avatar, { borderColor: paperTheme.colors.primary }]}
+                        />
+                    </TouchableOpacity>
+
+                    <Text style={[styles.linkText, { color: paperTheme.colors.onBackground }]}>Alterar foto</Text>
+
+                    <TextInput
+                        label='Nome'
+                        placeholder="Jhon Doe"
+                        keyboardType="default"
+                        autoCapitalize="words"
+                        mode="flat"
+                        value={name}
+                        onChangeText={setName}
+                        style={{ marginBottom: 60 }}
+                    />
+
+                    <Button mode="contained" onPress={handleUpdateProfile}>
+                        Salvar
+                    </Button>
+                </View>
+            </BlurView>
+        </Modal>
+    );
+}
+
+const styles = StyleSheet.create({
+    blurContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalContent: {
+        position: 'relative',
+        width: '90%',
+        borderRadius: 12,
+        padding: 20,
+        elevation: 4,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 2 },
+    },
+    avatar: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 10,
+        borderWidth: 2,
+    },
+
+    linkText: {
+        fontWeight: '500',
+        textAlign: 'center',
+        marginBottom: 16,
+    },
+
+});
